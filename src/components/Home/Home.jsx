c86b419886bf3da6240523547f1ec4b421d9a6d3
Geovanna Lessa <geovannalessa2@gmail.com>
2022-12-19 22:20:30 -0300

    Adiciondo imagens de produtos e campo de pesquisa

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,46 @@
 import styles from './Home.module.css'
+
 const Home = ()=>{
     return(
-        <div className={styles.principal}>
-            uepaaaaaaaaaa
-        </div>
+        <section>
+            <article className={styles.principal}>
+            <form>
+            <input className={styles.campo} type="text"/>
+            <button className={styles.botao}>
+                <img src='https://www.reshot.com/preview-assets/icons/Z45ULFPVWG/search-Z45ULFPVWG.svg'/>
+            </button>
+            </form>
+            </article>
+            <article className={styles.cards}>
+                <section>
+                    <img src="https://images.pexels.com/photos/708777/pexels-photo-708777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/708777/pexels-photo-708777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/708777/pexels-photo-708777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/1435301/pexels-photo-1435301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/1435301/pexels-photo-1435301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/1435301/pexels-photo-1435301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/143133/pexels-photo-143133.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+                <section>
+                    <img src="https://images.pexels.com/photos/708777/pexels-photo-708777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="frutas" />
+                </section>
+            </article>
+            <footer>
+                footer
+            </footer>
+        </section>
     )
 }
 export default Home;
